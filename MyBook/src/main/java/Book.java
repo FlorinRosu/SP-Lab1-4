76c76a7cdeac61dcbd1e523f32ea877230839579
Valentin Negru <negru.valentin1795@gmail.com>
2021-10-11 12:13:18 +0300

    Lab2 implementation

--- MyBook/src/main/java/Book.java
+++ MyBook/src/main/java/Book.java
@@ -2,31 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
-    private String name;
-    private List<String> paragraphs = new ArrayList<>();
-    private List<String> images = new ArrayList<>();
-    private List<String> tables = new ArrayList<>();
+    private final String name;
+    private final List<Author> authors = new ArrayList<>();
+    private final List<Chapter> chapters = new ArrayList<>();
+    private final List<SubChapter> subChapters = new ArrayList<>();
 
     public Book(String name) {
         this.name = name;
     }
 
-    public void createNewParagraph(String paragraphText) {
-        paragraphs.add(paragraphText);
+    public int createChapter(String chapter) {
+        chapters.add(new Chapter(chapter));
+        return chapters.size();
     }
 
-    public void createNewImage(String imageText) {
-        images.add(imageText);
-    }
+    public void addAuthor(Author author) { authors.add(author); }
 
-    public void createNewTable(String tableText) {
-        tables.add(tableText);
+    public Chapter getChapter(int chapterIndex) {
+        return chapters.get(chapterIndex);
     }
 
     public void print() {
         System.out.println(name);
-        System.out.println(paragraphs);
-        System.out.println(images);
-        System.out.println(tables);
+        System.out.println(chapters);
+        System.out.println(subChapters);
     }
 }
---
+++ MyBook/src/main/java/Chapter.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chapter {
+    private String name;
+    private final List<SubChapter> subChapters = new ArrayList<>();
+
+    public Chapter(String chapterText) {
+        name = chapterText;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int createSubChapter(String subChapterName) {
+        subChapters.add(new SubChapter(subChapterName));
+        return subChapters.size();
+    }
+
+    public SubChapter getSubChapter(int subchapterIndex) {
+        return subChapters.get(subchapterIndex);
+    }
+}
